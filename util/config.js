25d651e388d97bb90ad101b3ac6f225841143787
Abdalla-Fayez-Hamdan <61841621+Abdalla-Fayez-Hamdan@users.noreply.github.com>
2020-05-24 07:09:29 +0300

    social media API

---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAItvoH5VE07Y-juSjGP-TutfRLwtHbwKQ",
+    authDomain: "socialcomunity-eb92b.firebaseapp.com",
+    databaseURL: "https://socialcomunity-eb92b.firebaseio.com",
+    projectId: "socialcomunity-eb92b",
+    storageBucket: "socialcomunity-eb92b.appspot.com",
+    messagingSenderId: "589706757869",
+    appId: "1:589706757869:web:47db4614250d0bc765aee4",
+    measurementId: "G-TXC5QX4LYZ"
+  };
